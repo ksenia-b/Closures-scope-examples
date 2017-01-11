ae05a9445874a9247009910bc687f9853475bbad
Ksenia <ksenia.barshchyk@gmail.com>
2017-01-11 23:46:33 +0200

    Yahoo JavaScript Module Pattern

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-/**
- * Created by Ксюша on 10.01.2017.
- */
-
-// На момент инициализации, до выполнения кода:
-// window = { f: function, a: undefined, g: undefined }
-
-var a = 5;
-// window = { f: function, a: 5, g: undefined }
-
-function f(arg) { /*...*/ }
-// window = { f: function, a: 5, g: undefined } без изменений, f обработана ранее
-
-var g = function(arg) { /*...*/ };
-// window = { f: function, a: 5, g: function }
+var person = function () {
+// Private
+	var name = "Robert";
+	return {
+		getName : function () {
+		return name;
+	},
+	setName : function (newName) {
+		name = newName;
+	}
+};
+}();
+alert(person.name); // Undefined
+alert(person.getName()); // "Robert"
+person.setName("Robert Nyman");
+alert(person.getName()); // "Robert Nyman"
